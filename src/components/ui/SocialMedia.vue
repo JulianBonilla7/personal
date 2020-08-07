b6e0aac9a03eea1512bfcd8fa87dcfa20ea0bffd
Julián Bonilla <jfbonillago@gmail.com>
2020-08-07 13:01:57 -0500

    Added social media component

---
+++ src/components/ui/SocialMedia.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <div class="buttons is-centered has-addons mb-1">
+      <a
+        v-for="item in socials"
+        :key="item.name"
+        :href="item.url"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="button is-text is-outlined is-rounded is-medium"
+      >
+        <span class="icon">
+          <font-awesome-icon :icon="['fab', item.icon]"></font-awesome-icon>
+        </span>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      socials: [
+        {
+          name: "github",
+          url: "https://github.com/JulianBonilla7",
+          icon: "github"
+        },
+        {
+          name: "linkedin",
+          url: "https://www.linkedin.com/in/juli%C3%A1n-bonilla/",
+          icon: "linkedin-in"
+        },
+        {
+          name: "twitter",
+          url: "https://twitter.com/JulianBoni11a",
+          icon: "twitter"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
